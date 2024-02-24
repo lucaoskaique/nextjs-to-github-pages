b6eb5e3554508badab7c10c4812565169f2f8ea6
lucaoskaique <lucas.kaique175@hotmail.com>
2024-02-24 19:00:56 -0300

    Adiciona basePath e output ao next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/nextjs-to-github-pages",
+  output: "export",
   reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
