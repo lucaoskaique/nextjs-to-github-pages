3b5d97ea3add939ef9a90a786545b397dd699171
lucaoskaique <lucas.kaique175@hotmail.com>
2024-02-24 19:06:32 -0300

    Adiciona next.config.js com configurações para Next.js e GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/nextjs-to-github-pages",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
