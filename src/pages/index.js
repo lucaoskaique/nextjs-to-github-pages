b378a10d5f6957e0029fbba903fabe08413f03f3
lucaoskaique <lucas.kaique175@hotmail.com>
2024-02-24 19:16:15 -0300

    Atualiza caminhos das imagens

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
             >
               By{" "}
               <Image
-                src="/vercel.svg"
+                src="/nextjs-to-github-pages/vercel.svg"
                 alt="Vercel Logo"
                 className={styles.vercelLogo}
                 width={100}
@@ -42,7 +42,7 @@ export default function Home() {
         <div className={styles.center}>
           <Image
             className={styles.logo}
-            src="/next.svg"
+            src="/nextjs-to-github-pages/next.svg"
             alt="Next.js Logo"
             width={180}
             height={37}
