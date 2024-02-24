759eacd70586d7efaa6bd843503878b7a0c115bc
lucaoskaique <lucas.kaique175@hotmail.com>
2024-02-24 19:06:39 -0300

    Removido next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/nextjs-to-github-pages",
-  output: "export",
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
